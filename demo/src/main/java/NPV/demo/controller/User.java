85a61c74686d7b8a0a1502f8bdff030d6b0c7b2c
Seonguk <xsungwook3x@naver.com>
2021-12-20 23:13:29 +0900

    [시작] 프로젝트 시작

---
+++ demo/src/main/java/NPV/demo/controller/User.java
@@ -0,0 +1,10 @@
+package NPV.demo.controller;
+
+public class User {
+
+    private String name;
+    private String password;
+    private String serialNumber;
+    private String phoneNumber;
+
+}
